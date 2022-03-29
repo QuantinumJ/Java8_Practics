067364b02553180f5290e054203186818e8d24c0
QuantinumJ <eduardvarnavskiy@yahoo.es>
2022-03-29 09:04:48 +0200

    first commit

---
+++ src/aprenderemosAfondo/Cliente.java
@@ -0,0 +1,23 @@
+package aprenderemosAfondo;
+
+public class Cliente extends Persona {
+	
+	/*
+	 * A - administracion
+	 * E - empresa
+	 * P - particular*/
+private char tipo;
+
+public char getTipo() {
+	return tipo;
+}
+public void setTipo(char t) {
+	tipo=t;
+}
+	Cliente c = new Cliente();
+	
+
+
+
+
+}
--- src/aprenderemosAfondo/Persona.java
+++ src/aprenderemosAfondo/Persona.java
@@ -1,10 +1,66 @@
 package aprenderemosAfondo;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+// Creacion de clase 
 public class Persona {
-
+	// Definicion de las variables o atributos de la clase 
+	
+	private String nombre;
+	private String apellido;
+	private LocalDate fecha_nacimiento;
+	
+	// Campo privado presentado para el numero de persona
+	private  int numero;
+	// Campo estatico presentado para el contador de personas
+	private static int numInstancias;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public static int getNumInstancias() {
+		return numInstancias;
+	}
+	// metodo main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
+	// constructor
+	public Persona() {
+		nombre = "";
+		apellido= "";
+		fecha_nacimiento=null;
+		numInstancias++;
+		numero = numInstancias;
+		
+	}
+	public Persona(String a,String n, LocalDate nac) {
+		nombre=n;
+		apellido=a;
+		fecha_nacimiento = nac;
+		// Crecimiento de numero de instacia y asignacion del numero
+		
+	}
+	// metodos  Accesores get y set
+	public String getNombre() {
+		return nombre;
+	}
 
+	public void setNombre(String nombre) {
+	nombre = nombre.toLowerCase();
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		apellido = apellido.toUpperCase();
+	}
+	
+ //metodos generales
+	
+	public long calculoEdad() {
+		return fecha_nacimiento.until(LocalDate.now(),ChronoUnit.YEARS);
+	}
 }
